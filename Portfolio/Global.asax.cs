c57c3c9902cc47e19a3f7a9a5a14b5c7c17963e7
Your Name <your@example.com>
2016-05-26 12:54:10 -0400

    initial setup

---
+++ Portfolio/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+//using System.Data.Entity;
+//using Portfolio.Models;
+
+namespace Portfolio
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //Database.SetInitializer<Portfolio_ProjectEntities>(new DropCreateDatabaseIfModelChanges<Portfolio_ProjectEntities>());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
